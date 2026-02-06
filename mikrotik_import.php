2cb920d5dd02df64c5e2968afb795af75df48543
freedarwuin <ingdarwuin@gmail.com>
2026-02-06 00:49:30 -0400

    Update

--- mikrotik_import.php
+++ mikrotik_import.php
@@ -1,3 +1,5 @@
+Estoy notando que no se esta extrayendo el burst del mikrotik aqui te paso el codigo
+
 <?php
 
 use PEAR2\Net\RouterOS;
@@ -10,7 +12,8 @@ function mikrotik_import_ui()
     _admin();
     $ui->assign('_title', 'Mikrotik Import');
     $ui->assign('_system_menu', 'settings');
-    $ui->assign('_admin', Admin::_info());
+    $admin = Admin::_info();
+    $ui->assign('_admin', $admin);
     $ui->display('mikrotik_import.tpl');
 }
 
@@ -20,192 +23,211 @@ function mikrotik_import_start_ui()
     ini_set('max_execution_time', 0);
     set_time_limit(0);
     _admin();
-
     $ui->assign('_title', 'Mikrotik Start Import');
     $ui->assign('_system_menu', 'settings');
-    $ui->assign('_admin', Admin::_info());
+    $admin = Admin::_info();
+    $ui->assign('_admin', $admin);
 
     $type = $_POST['type'];
-    $mikrotik = ORM::for_table('tbl_routers')
-        ->where('name', $_POST['server'])
-        ->find_one();
 
-    if ($type === 'Hotspot') {
-        $results = mikrotik_import_mikrotik_hotspot_package(
-            $_POST['server'],
-            $mikrotik['ip_address'],
-            $mikrotik['username'],
-            $mikrotik['password']
-        );
-    } else {
-        $results = mikrotik_import_mikrotik_ppoe_package(
-            $_POST['server'],
-            $mikrotik['ip_address'],
-            $mikrotik['username'],
-            $mikrotik['password']
-        );
+    // get mikrotik info
+    $mikrotik = ORM::for_table('tbl_routers')->where('name', $_POST['server'])->find_one();
+    if($type=='Hotspot'){
+        $results = mikrotik_import_mikrotik_hotspot_package($_POST['server'], $mikrotik['ip_address'], $mikrotik['username'], $mikrotik['password']);
+    }else if($type=='PPPOE'){
+        $results = mikrotik_import_mikrotik_ppoe_package($_POST['server'], $mikrotik['ip_address'], $mikrotik['username'], $mikrotik['password']);
     }
-
     $ui->assign('results', $results);
     $ui->display('mikrotik_import_start.tpl');
 }
 
-/* ================= HOTSPOT ================= */
-
 function mikrotik_import_mikrotik_hotspot_package($router, $ip, $user, $pass)
 {
     $client = Mikrotik::getClient($ip, $user, $pass);
-    $profiles = $client->sendSync(
-        new RouterOS\Request('/ip hotspot user profile print')
-    )->toArray();
-
+    // import Hotspot Profile to package
+    $printRequest = new RouterOS\Request(
+        '/ip hotspot user profile print'
+    );
     $results = [];
-
+    $profiles = $client->sendSync($printRequest)->toArray();
     foreach ($profiles as $p) {
-
         $name = $p->getProperty('name');
-        $rateLimitRaw = trim($p->getProperty('rate-limit'));
+        $rateLimit = $p->getProperty('rate-limit');
         $sharedUser = $p->getProperty('shared-user');
 
-        if (empty($rateLimitRaw)) {
-            continue;
-        }
+        // 10M/10M
+        $rateLimit = explode(" ", $rateLimit)[0];
+        if (strlen($rateLimit) > 1) {
+            // Create Bandwidth profile
+            $rate = explode("/", $rateLimit);
+            $unit_up = preg_replace("/[^a-zA-Z]+/", "", $rate[0]) . "bps";
+            $unit_down = preg_replace("/[^a-zA-Z]+/", "", $rate[1]) . "bps";
+            $rate_up = preg_replace("/[^0-9]+/", "", $rate[0]);
+            $rate_down = preg_replace("/[^0-9]+/", "", $rate[1]);
+            $bw_name = str_replace("/", "_", $rateLimit);
+            $bw = ORM::for_table('tbl_bandwidth')->where('name_bw', $bw_name)->find_one();
+            if (!$bw) {
+                $results[] = "Ancho de banda creado: $bw_name";
+                $d = ORM::for_table('tbl_bandwidth')->create();
+                $d->name_bw = $bw_name;
+                $d->rate_down = $rate_down;
+                $d->rate_down_unit = $unit_down;
+                $d->rate_up = $rate_up;
+                $d->rate_up_unit = $unit_up;
+                $d->save();
+                $bw_id = $d->id();
+            }else{
+                $results[] = "El ancho de banda existe: $bw_name";
+                $bw_id = $bw->id;
+            }
 
-        // rate-limit completo (NO se corta)
-        $parts = preg_split('/\s+/', $rateLimitRaw);
-        $maxLimit = $parts[0]; // Ej: 10M/10M
-
-        [$up, $down] = explode('/', $maxLimit);
-
-        $rate_up = preg_replace('/\D/', '', $up);
-        $rate_down = preg_replace('/\D/', '', $down);
-        $unit_up = preg_replace('/\d/', '', $up) . 'bps';
-        $unit_down = preg_replace('/\d/', '', $down) . 'bps';
-
-        // Nombre único incluyendo burst
-        $bw_name = str_replace(['/', ' '], ['_', '__'], $rateLimitRaw);
-
-        $bw = ORM::for_table('tbl_bandwidth')
-            ->where('name_bw', $bw_name)
-            ->find_one();
-
-        if (!$bw) {
-            $d = ORM::for_table('tbl_bandwidth')->create();
-            $d->name_bw = $bw_name;
-            $d->rate_down = $rate_down;
-            $d->rate_down_unit = $unit_down;
-            $d->rate_up = $rate_up;
-            $d->rate_up_unit = $unit_up;
-            $d->save();
-            $bw_id = $d->id();
-            $results[] = "Ancho de banda creado: $bw_name";
-        } else {
-            $bw_id = $bw->id;
-        }
-
-        $pack = ORM::for_table('tbl_plans')
-            ->where('name_plan', $name)
-            ->find_one();
-
-        if (!$pack) {
-            $d = ORM::for_table('tbl_plans')->create();
-            $d->name_plan = $name;
-            $d->id_bw = $bw_id;
-            $d->price = '1';
-            $d->type = 'Hotspot';
-            $d->typebp = 'Unlimited';
-            $d->limit_type = 'Time_Limit';
-            $d->time_limit = 0;
-            $d->time_unit = 'Hrs';
-            $d->data_limit = 0;
-            $d->data_unit = 'MB';
-            $d->validity = '1';
-            $d->validity_unit = 'Months';
-            $d->shared_users = $sharedUser;
-            $d->routers = $router;
-            $d->enabled = 1;
-            $d->save();
-            $results[] = "Paquete creado: $name";
+            // Create Packages
+            $pack = ORM::for_table('tbl_plans')->where('name_plan', $name)->find_one();
+            if(!$pack){
+                $results[] = "Paquetes creados: $name";
+                $d = ORM::for_table('tbl_plans')->create();
+                $d->name_plan = $name;
+                $d->id_bw = $bw_id;
+                $d->price = '1';
+                $d->type = 'Hotspot';
+                $d->typebp = 'Unlimited';
+                $d->limit_type = 'Time_Limit';
+                $d->time_limit = 0;
+                $d->time_unit = 'Hrs';
+                $d->data_limit = 0;
+                $d->data_unit = 'MB';
+                $d->validity = '1';
+                $d->validity_unit = 'Months';
+                $d->shared_users = $sharedUser;
+                $d->routers = $router;
+                $d->enabled = 1;
+                $d->save();
+            }else{
+                $results[] = "Los paquetes existen: $name";
+            }
         }
     }
-
+    // Import user
+    $userRequest = new RouterOS\Request(
+        '/ip hotspot user print'
+    );
+    $users = $client->sendSync($userRequest)->toArray();
+    foreach ($users as $u) {
+        $username = $u->getProperty('name');
+        if(!empty($username) && !empty($u->getProperty('password'))){
+            $d = ORM::for_table('tbl_customers')->where('username', $username)->find_one();
+            if($d){
+                $results[] = "El nombre de usuario existe: $username";
+            }else{
+                $d = ORM::for_table('tbl_customers')->create();
+                $d->username = $username;
+                $d->password = $u->getProperty('password');
+                $d->fullname = $username;
+                $d->address = '';
+                $d->email = ($u->getProperty('email'))?$u->getProperty('email'):'';
+                $d->phonenumber = '';
+                if ($d->save()) {
+                    $results[] = "$username añadido con éxito";
+                }else{
+                    $results[] = "$username No se pudo agregar";
+                }
+            }
+        }
+    }
     return $results;
 }
 
-/* ================= PPPOE ================= */
 
 function mikrotik_import_mikrotik_ppoe_package($router, $ip, $user, $pass)
 {
     $client = Mikrotik::getClient($ip, $user, $pass);
-    $profiles = $client->sendSync(
-        new RouterOS\Request('/ppp profile print')
-    )->toArray();
-
+    // import Hotspot Profile to package
+    $printRequest = new RouterOS\Request(
+        '/ppp profile print'
+    );
     $results = [];
-
+    $profiles = $client->sendSync($printRequest)->toArray();
     foreach ($profiles as $p) {
-
         $name = $p->getProperty('name');
-        $rateLimitRaw = trim($p->getProperty('rate-limit'));
+        $rateLimit = $p->getProperty('rate-limit');
 
-        if (empty($rateLimitRaw)) {
-            continue;
-        }
+        // 10M/10M
+        $rateLimit = explode(" ", $rateLimit)[0];
+        if (strlen($rateLimit) > 1) {
+            // Create Bandwidth profile
+            $rate = explode("/", $rateLimit);
+            $unit_up = preg_replace("/[^a-zA-Z]+/", "", $rate[0]) . "bps";
+            $unit_down = preg_replace("/[^a-zA-Z]+/", "", $rate[1]) . "bps";
+            $rate_up = preg_replace("/[^0-9]+/", "", $rate[0]);
+            $rate_down = preg_replace("/[^0-9]+/", "", $rate[1]);
+            $bw_name = str_replace("/", "_", $rateLimit);
+            $bw = ORM::for_table('tbl_bandwidth')->where('name_bw', $bw_name)->find_one();
+            if (!$bw) {
+                $results[] = "Ancho de banda creado: $bw_name";
+                $d = ORM::for_table('tbl_bandwidth')->create();
+                $d->name_bw = $bw_name;
+                $d->rate_down = $rate_down;
+                $d->rate_down_unit = $unit_down;
+                $d->rate_up = $rate_up;
+                $d->rate_up_unit = $unit_up;
+                $d->save();
+                $bw_id = $d->id();
+            }else{
+                $results[] = "El ancho de banda existe: $bw_name";
+                $bw_id = $bw->id;
+            }
 
-        $parts = preg_split('/\s+/', $rateLimitRaw);
-        $maxLimit = $parts[0];
-
-        [$up, $down] = explode('/', $maxLimit);
-
-        $rate_up = preg_replace('/\D/', '', $up);
-        $rate_down = preg_replace('/\D/', '', $down);
-        $unit_up = preg_replace('/\d/', '', $up) . 'bps';
-        $unit_down = preg_replace('/\d/', '', $down) . 'bps';
-
-        $bw_name = str_replace(['/', ' '], ['_', '__'], $rateLimitRaw);
-
-        $bw = ORM::for_table('tbl_bandwidth')
-            ->where('name_bw', $bw_name)
-            ->find_one();
-
-        if (!$bw) {
-            $d = ORM::for_table('tbl_bandwidth')->create();
-            $d->name_bw = $bw_name;
-            $d->rate_down = $rate_down;
-            $d->rate_down_unit = $unit_down;
-            $d->rate_up = $rate_up;
-            $d->rate_up_unit = $unit_up;
-            $d->save();
-            $bw_id = $d->id();
-            $results[] = "Ancho de banda creado: $bw_name";
-        } else {
-            $bw_id = $bw->id;
-        }
-
-        $pack = ORM::for_table('tbl_plans')
-            ->where('name_plan', $name)
-            ->find_one();
-
-        if (!$pack) {
-            $d = ORM::for_table('tbl_plans')->create();
-            $d->name_plan = $name;
-            $d->id_bw = $bw_id;
-            $d->price = '1';
-            $d->type = 'PPPOE';
-            $d->typebp = 'Unlimited';
-            $d->limit_type = 'Time_Limit';
-            $d->time_limit = 0;
-            $d->time_unit = 'Hrs';
-            $d->data_limit = 0;
-            $d->data_unit = 'MB';
-            $d->validity = '1';
-            $d->validity_unit = 'Months';
-            $d->routers = $router;
-            $d->enabled = 1;
-            $d->save();
-            $results[] = "Paquete creado: $name";
+            // Create Packages
+            $pack = ORM::for_table('tbl_plans')->where('name_plan', $name)->find_one();
+            if(!$pack){
+                $results[] = "Paquetes creados: $name";
+                $d = ORM::for_table('tbl_plans')->create();
+                $d->name_plan = $name;
+                $d->id_bw = $bw_id;
+                $d->price = '1';
+                $d->type = 'PPPOE';
+                $d->typebp = 'Unlimited';
+                $d->limit_type = 'Time_Limit';
+                $d->time_limit = 0;
+                $d->time_unit = 'Hrs';
+                $d->data_limit = 0;
+                $d->data_unit = 'MB';
+                $d->validity = '1';
+                $d->validity_unit = 'Months';
+                $d->routers = $router;
+                $d->enabled = 1;
+                $d->save();
+            }else{
+                $results[] = "Los paquetes existen: $name";
+            }
         }
     }
-
+    // Import user
+    $userRequest = new RouterOS\Request(
+        '/ppp secret print'
+    );
+    $users = $client->sendSync($userRequest)->toArray();
+    foreach ($users as $u) {
+        $username = $u->getProperty('name');
+        if(!empty($username) && !empty($u->getProperty('password'))){
+            $d = ORM::for_table('tbl_customers')->where('username', $username)->find_one();
+            if($d){
+                $results[] = "El nombre de usuario existe: $username";
+            }else{
+                $d = ORM::for_table('tbl_customers')->create();
+                $d->username = $username;
+                $d->password = $u->getProperty('password');
+                $d->fullname = $username;
+                $d->address = '';
+                $d->email = '';
+                $d->phonenumber = '';
+                if ($d->save()) {
+                    $results[] = "$username añadido con éxito";
+                }else{
+                    $results[] = "$username No se pudo agregar";
+                }
+            }
+        }
+    }
     return $results;
 }
